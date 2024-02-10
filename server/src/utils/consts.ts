2655f0811e016547e4a6c47a7b3899ae76ba9fe7
dmytro-chushko <dmytro.chushko@gmail.com>
2024-02-10 22:28:55 +0200

    Init server

---
+++ server/src/utils/consts.ts
@@ -0,0 +1,17 @@
+export const enum SUCCESSFUL_RESPONSE {
+  UPDATED = 'Entity successfully updated',
+  DELETED = 'Entity successfully deleted',
+}
+
+export const enum ROUTE {
+  BOARD = 'board',
+  CARD = 'card',
+  ALL = 'all',
+  ORDER = 'order',
+  PARAM_ID = '/:id',
+}
+
+export const enum ORDER_ACTION {
+  INCREMENT = 'increment',
+  DECREMENT = 'decrement',
+}
---
+++ server/src/utils/error-messages.ts
@@ -0,0 +1,10 @@
+export const enum VALIDATION_ERROR {
+  IS_STRING = 'Must be a string',
+  IS_NUMBER = 'Must be a number',
+  IS_ENUM = 'Must be one of the',
+  MAX_LENGTH = "Can't be more than",
+}
+
+export const enum EXCEPTION_MESSAGE {
+  NOT_FOUND = 'Entity not found',
+}
